ab77c12f4f9af7d4a1b7a9fd2a153ba10c21476e
Unknown <chentenial@gmail.com>
2018-05-17 18:36:59 +0200

    New Ftp sync setup

---
+++ functions.php
@@ -0,0 +1,173 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentysixteen-style' for the Twenty Sixteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+//Google Fonts
+function twentysixteen_fonts_url() {
+  $fonts_url = '';
+  $fonts = array();
+  $subsets = 'latin,latin-ext';
+
+  $fonts[] = 'Mukta+Mahee:400,600,800';
+
+  if ( $fonts ) {
+    $fonts_url = add_query_arg( array(
+      'family' => urlencode( implode( '|', $fonts ) ),
+      'subset' => urlencode( $subsets ),
+    ), 'https://fonts.googleapis.com/css' );
+  }
+
+  return $fonts_url;
+}
+
+
+//Suche
+function fb_add_search_box ( $items, $args ) {
+
+       // only on primary menu
+       if( 'primary' === $args -> theme_location )
+             $items .= '<li class="menu-item menu-item-search">' . get_search_form( FALSE ) . '</li>';
+
+       return $items;
+ }
+ add_filter( 'wp_nav_menu_items', 'fb_add_search_box', 10, 2 );
+
+
+
+
+function sgr_show_current_cat_on_single($output) {
+     global $post;
+     if( is_single() ) {
+          $categories = wp_get_post_categories($post->ID);
+          foreach( $categories as $catid ) {
+    $cat = get_category($catid);
+
+         // Find cat-item-ID in the string
+         if(preg_match('#cat-item-' . $cat->cat_ID . '#', $output)) {
+              $output = str_replace('cat-item-'.$cat->cat_ID, 'cat-item-'.$cat->cat_ID . ' current-cat', $output);
+         }
+          }
+
+     }
+     return $output;
+}
+
+add_filter('wp_list_categories', 'sgr_show_current_cat_on_single');
+
+
+function remove_more_link_scroll( $link ) {
+  $link = preg_replace( '|#more-[0-9]+|', '', $link );
+  return $link;
+}
+add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
+
+//Breadcrumb
+function nav_breadcrumb() {
+
+ $delimiter = '&raquo;';
+ $home = 'Home';
+ $before = '<span class="current-page">';
+ $after = '</span>';
+
+ if ( !is_home() && !is_front_page() || is_paged() ) {
+
+ echo '<nav class="breadcrumb">Sie sind hier: ';
+
+ global $post;
+ $homeLink = get_bloginfo('url');
+ echo '<a href="' . $homeLink . '">' . $home . '</a> ' . $delimiter . ' ';
+
+ if ( is_category()) {
+ global $wp_query;
+ $cat_obj = $wp_query->get_queried_object();
+ $thisCat = $cat_obj->term_id;
+ $thisCat = get_category($thisCat);
+ $parentCat = get_category($thisCat->parent);
+ if ($thisCat->parent != 0) echo(get_category_parents($parentCat, TRUE, ' ' . $delimiter . ' '));
+ echo $before . single_cat_title('', false) . $after;
+
+ } elseif ( is_day() ) {
+ echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
+ echo '<a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a> ' . $delimiter . ' ';
+ echo $before . get_the_time('d') . $after;
+
+ } elseif ( is_month() ) {
+ echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
+ echo $before . get_the_time('F') . $after;
+
+ } elseif ( is_year() ) {
+ echo $before . get_the_time('Y') . $after;
+
+ } elseif ( is_single() && !is_attachment() ) {
+ if ( get_post_type() != 'post' ) {
+ $post_type = get_post_type_object(get_post_type());
+ $slug = $post_type->rewrite;
+ echo '<a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a> ' . $delimiter . ' ';
+ echo $before . get_the_title() . $after;
+ } else {
+ $cat = get_the_category(); $cat = $cat[0];
+ echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
+ echo $before . get_the_title() . $after;
+ }
+
+ } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
+ $post_type = get_post_type_object(get_post_type());
+ echo $before . $post_type->labels->singular_name . $after;
+
+
+ } elseif ( is_attachment() ) {
+ $parent = get_post($post->post_parent);
+ $cat = get_the_category($parent->ID); $cat = $cat[0];
+ echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
+ echo '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a> ' . $delimiter . ' ';
+ echo $before . get_the_title() . $after;
+
+ } elseif ( is_page() && !$post->post_parent ) {
+ echo $before . get_the_title() . $after;
+
+ } elseif ( is_page() && $post->post_parent ) {
+ $parent_id = $post->post_parent;
+ $breadcrumbs = array();
+ while ($parent_id) {
+ $page = get_page($parent_id);
+ $breadcrumbs[] = '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>';
+ $parent_id = $page->post_parent;
+ }
+ $breadcrumbs = array_reverse($breadcrumbs);
+ foreach ($breadcrumbs as $crumb) echo $crumb . ' ' . $delimiter . ' ';
+ echo $before . get_the_title() . $after;
+
+ } elseif ( is_search() ) {
+ echo $before . 'Ergebnisse für Ihre Suche nach "' . get_search_query() . '"' . $after;
+
+ } elseif ( is_tag() ) {
+ echo $before . 'Beiträge mit dem Schlagwort "' . single_tag_title('', false) . '"' . $after;
+
+ } elseif ( is_404() ) {
+ echo $before . 'Fehler 404' . $after;
+ }
+
+ if ( get_query_var('paged') ) {
+ if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
+ echo ': ' . __('Seite') . ' ' . get_query_var('paged');
+ if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
+ }
+
+ echo '</nav>';
+
+ }
+} 
+
+
+?>
